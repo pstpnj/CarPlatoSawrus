10c43cefb1415db753b48cb19abac40199af53e8
PNJ <its0oskar@gmail.com>
2024-07-24 17:39:14 +0700

    fix plate detection bug

--- Plate Annotation/plate_detect.py
+++ Plate Annotation/plate_detect.py
@@ -19,20 +19,21 @@ def plate_detection(img_src):
         data = plate.boxes.data.tolist()
         for box in data:
             # draw border around the car
-            box = data[:4]
-            # img = cv2.rectangle(img, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (0, 255, 0), 1)
-            cv2.rectangle(img, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (255, 0, 0), 1)
+            img = cv2.rectangle(img, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (255, 0, 0), 1)
+            # cv2.rectangle(img, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (255, 0, 0), 1)
+            
             plate = img[int(box[1]):int(box[3]), int(box[0]):int(box[2])]
             output.append(Image.fromarray(plate))
-            
+    
     return output
-
-
+            
+            
 if __name__ == '__main__':
     a = car_detection('./pic/452533647_1716748245756349_1609499174989890082_n.png')
-    # b = plate_detection(a[0])
+    b = plate_detection(a[0])
     
-    a[0].show()
-    # b[0].show()
+    # a[0].show()
+    b[0].show()
+    # print(b)
     
 # /Users/pstpnj/Desktop/CarPlatoSawrus/Dataset/Car_plate/hatchback/PHOTO_6.jpg
