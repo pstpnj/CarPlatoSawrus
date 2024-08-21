6be0b034822146d163ebe74a189ef44a1243bf4f
PNJ <its0oskar@gmail.com>
2024-08-21 15:37:38 +0700

    developing more

--- Plate Annotation/car_detect.py
+++ Plate Annotation/car_detect.py
@@ -8,40 +8,38 @@ def car_detection(img_url):
     output = []
 
     # load the model
-    car_detect = YOLO('./yolov8x.pt') # Model for detecting the car
+    car_detect = YOLO('./yolov8n.pt') # Model for detecting the car
 
     img_src = Image.open(img_url)
     img = np.array(img_src)
 
-    # detect the car using pre-trained model
-    vehicles = car_detect.predict(img_src, conf=0.4)
-
     # define id for each vehicle
     vehicles_id = [2, 3, 5, 7]
+    
+    # detect the car using pre-trained model
+    vehicles = car_detect.predict(img_src, conf=0.5, classes=vehicles_id)
 
     for veh in vehicles:
         # get data of the vehicle
         datas = veh.boxes.data.tolist()
         # print(data)
         for data in datas:
-            # get the id of the vehicle
-            id = data[5]
-            if id in vehicles_id:
-                box = data[:4]
-                # draw border around the car
-                img = cv2.rectangle(img, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (0, 255, 0), 1)
-                # crop the car
-                car = img[int(box[1]):int(box[3]), int(box[0]):int(box[2])]
-
-                # Whole Picture with car border
-                # output.append(Image.fromarray(img))
-                # Picture of each car
-                output.append(Image.fromarray(car))
+            box = data[:4]
+            # draw border around the car
+            # img = cv2.rectangle(img, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (0, 255, 0), 1)
+            
+            # crop the car
+            car = img[int(box[1]):int(box[3]), int(box[0]):int(box[2])]
+            
+			# Resize 
+            # car = cv2.resize(car, (1000,1000))
+            
+            # Picture of each car
+            output.append(Image.fromarray(car))
                 
     return output
 
 if __name__ == '__main__':
-    output = car_detection('./pic/452533647_1716748245756349_1609499174989890082_n.png')
-    output[0].show()
-    # for i in output:
-    #     i.show()
+    output = car_detection('./pic/bangkok-traffic-1.jpg')
+    for i in output:
+        i.show()
--- Plate Annotation/plate_detect.py
+++ Plate Annotation/plate_detect.py
@@ -8,10 +8,9 @@ from PIL import Image
 
 def plate_detection(img_src):
     plate_detect = YOLO("./license_plate_detector.pt")
-    # img_src = Image.open(img_url)
     img = np.array(img_src)
     
-    plates = plate_detect.predict(img_src, conf=0.4)
+    plates = plate_detect.predict(img_src, conf=0.7)
     
     output = []
     
@@ -19,10 +18,16 @@ def plate_detection(img_src):
         data = plate.boxes.data.tolist()
         for box in data:
             # draw border around the car
-            img = cv2.rectangle(img, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (255, 0, 0), 1)
-            # cv2.rectangle(img, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (255, 0, 0), 1)
+            # img = cv2.rectangle(img, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (255, 0, 0), 1)
             
             plate = img[int(box[1]):int(box[3]), int(box[0]):int(box[2])]
+            # Resize
+            plate = cv2.resize(plate, (1000,1000))
+            
+            # Crop only the plate
+            plate = cv2.cvtColor(plate, cv2.COLOR_BGR2GRAY)
+            _, plate = cv2.threshold(plate, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+            
             output.append(Image.fromarray(plate))
     
     return output
@@ -30,10 +35,9 @@ def plate_detection(img_src):
             
 if __name__ == '__main__':
     a = car_detection('./pic/452533647_1716748245756349_1609499174989890082_n.png')
+    print(len(a))
     b = plate_detection(a[0])
     
-    # a[0].show()
     b[0].show()
-    # print(b)
+ 
     
-# /Users/pstpnj/Desktop/CarPlatoSawrus/Dataset/Car_plate/hatchback/PHOTO_6.jpg
